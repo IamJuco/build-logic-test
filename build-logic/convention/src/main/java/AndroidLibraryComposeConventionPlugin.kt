b93704001d3cd6baab702d6ebbc1949a0dd1bc3b
Juseonghyun <jucoding99@gmail.com>
2025-08-03 15:40:40 +0900

    chore: compose가 필요한곳에 compose compiler 추가

--- build-logic/convention/src/main/java/AndroidLibraryComposeConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidLibraryComposeConventionPlugin.kt
@@ -9,6 +9,7 @@ class AndroidLibraryComposeConventionPlugin : Plugin<Project> {
         target.run {
             pluginManager.run {
                 apply("multi.module.android.library")
+                apply("org.jetbrains.kotlin.plugin.compose")
             }
 
             val extension = extensions.getByType<LibraryExtension>()
