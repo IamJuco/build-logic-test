30ae0823fa1c9fc06c65f1e07049dc0fbfe71749
Juseonghyun <jucoding99@gmail.com>
2025-08-03 15:22:14 +0900

    chore: extensionType이 Library인데 Application으로 설정된것을 Library로 정상 수정

--- build-logic/convention/src/main/java/com/example/convention/BuildTypes.kt
+++ build-logic/convention/src/main/java/com/example/convention/BuildTypes.kt
@@ -3,6 +3,7 @@ package com.example.convention
 import com.android.build.api.dsl.ApplicationExtension
 import com.android.build.api.dsl.BuildType
 import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.dsl.LibraryExtension
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
@@ -37,7 +38,7 @@ internal fun Project.configureBuildTypes(
             }
 
             ExtensionType.LIBRARY -> {
-                extensions.configure<ApplicationExtension> {
+                extensions.configure<LibraryExtension> {
                     buildTypes {
                         debug {
                             configureDebugBuildType(apiKey = apiKey)
